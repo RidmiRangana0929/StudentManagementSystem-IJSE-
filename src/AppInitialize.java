8ef02ed8d467b6f3416a14f4848da65622cbc35f
RidmiRangana0929 <ridmirangana1429@gmail.com>
2022-07-12 12:12:15 +0530

    updated AppInitialize

--- src/AppInitialize.java
+++ src/AppInitialize.java
@@ -3,6 +3,8 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class AppInitialize extends Application {
 
     public static void main(String[] args) {
@@ -10,7 +12,8 @@ public class AppInitialize extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-
+    public void start(Stage primaryStage) throws IOException {
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("view/studentManagementForm.fxml"))));
+        primaryStage.show();
     }
 }
