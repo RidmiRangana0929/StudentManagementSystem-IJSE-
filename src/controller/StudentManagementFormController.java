29cde9cf80cc52790b2f6ab6f49c2827d14905fe
RidmiRangana0929 <ridmirangana1429@gmail.com>
2022-07-12 11:33:31 +0530

    create ui

--- src/controller/StudentManagementFormController.java
+++ src/controller/StudentManagementFormController.java
@@ -1,4 +1,36 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class StudentManagementFormController {
+    public AnchorPane studentContext;
+    public TextField txtStudentId;
+    public TextField txtName;
+    public TextField txtEmail;
+    public TextField txtContact;
+    public TextField txtAddress;
+    public TextField txtNic;
+    public TableView tblStudent;
+    public TableColumn colStId;
+    public TableColumn colStName;
+    public TableColumn colEmail;
+    public TableColumn colContact;
+    public TableColumn colAddress;
+    public TableColumn colNic;
+
+    public void saveOnAction(ActionEvent actionEvent) {
+    }
+
+    public void updateOnAction(ActionEvent actionEvent) {
+    }
+
+    public void deleteOnAction(ActionEvent actionEvent) {
+    }
+
+    public void txtSearchOnAction(ActionEvent actionEvent) {
+    }
 }
