327b66abc95f123853f066da1382bf63e910f1f9
RidmiRangana0929 <ridmirangana1429@gmail.com>
2022-07-12 12:33:56 +0530

    update saveOnAction and txtSearchOnAction

--- src/controller/StudentManagementFormController.java
+++ src/controller/StudentManagementFormController.java
@@ -1,10 +1,16 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
+import model.Student;
+import util.CrudUtil;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 public class StudentManagementFormController {
     public AnchorPane studentContext;
@@ -23,6 +29,26 @@ public class StudentManagementFormController {
     public TableColumn colNic;
 
     public void saveOnAction(ActionEvent actionEvent) {
+
+        Student s = new Student(
+                txtStudentId.getText(),txtName.getText(),txtEmail.getText(),txtContact.getText(),txtAddress.getText(),txtNic.getText()
+        );
+        try {
+            if(CrudUtil.execute("INSERT INTO Student VALUES (?,?,?,?,?,?)",s.getStudent_id(),s.getStudent_name(),s.getEmail(),s.getContact(),s.getAddress(),s.getNic())){
+
+                new Alert(Alert.AlertType.CONFIRMATION,"Student Information saved Successfully...!").show();
+
+            }else {
+
+                new Alert(Alert.AlertType.WARNING,"Error! Try Again...").show();
+
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+
     }
 
     public void updateOnAction(ActionEvent actionEvent) {
@@ -31,6 +57,21 @@ public class StudentManagementFormController {
     public void deleteOnAction(ActionEvent actionEvent) {
     }
 
-    public void txtSearchOnAction(ActionEvent actionEvent) {
+    public void txtSearchOnAction(ActionEvent actionEvent) throws SQLException {
+
+        ResultSet result= null;
+        try {
+            result = CrudUtil.execute("SELECT * FROM Student WHERE student_id=?",txtStudentId.getText());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        if(result.next()){
+            txtName.setText(result.getString(1));
+            txtEmail.setText(result.getString(2));
+            txtContact.setText(result.getString(3));
+            txtAddress.setText(result.getString(4));
+            txtAddress.setText(result.getString(5));
+        }
+
     }
 }
